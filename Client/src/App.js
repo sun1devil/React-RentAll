c6d98694d79a590e8c565520bce713f3e51efc53
Linh Tong <linhthitong@gmail.com>
2018-06-28 17:30:10 -0700

    create App file

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import AccountPage from "./pages/AccountPage";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Nav/>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="accountpage" component= {AccountPage} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="search" component={Search} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;

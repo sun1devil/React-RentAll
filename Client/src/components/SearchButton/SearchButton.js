c50fd6caad320b55c7b5fae02d18c7b230836ec1
sun1devil <codejmc11@gmail.com>
2018-07-01 16:07:29 -0700

    add autocomplete for items and google places complete for location search boxes, also add submit button to capture values

---
+++ Client/src/components/SearchButton/SearchButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchButton = props => {
+console.log(props)
+return (
+<button value= {props}> Search </button>
+)
+}
+
+export default SearchButton
+    
+
+
+
+
+
+
+
